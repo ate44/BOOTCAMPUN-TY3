d49a9a0c35acb194ca4c2d4db2aa4897ae46050f
Beliz Yazıcı <beliz.yazici@std.izmirekonomi.edu.tr>
2024-07-02 16:29:17 +0300

    goblin ve troll için attack fonksiyonu eklendi

--- Assets/Beliz/Scripts/TrollController.cs
+++ Assets/Beliz/Scripts/TrollController.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TrollController : MonoBehaviour
 {
     public Animator animator;
-    public string isWalkingParameter = "walk"; 
-    public float minWaitTime = 2.0f; 
-    public float maxWaitTime = 5.0f; 
-    public float speed = 3.0f; 
-
+    public string isWalkingParameter = "walk";
+    public float minWaitTime = 2.0f;
+    public float maxWaitTime = 5.0f;
+    public float speed = 3.0f;
+    public string attackParameter = "attack03";
+    
     private bool isWalking = false;
+    private bool isPlayerDetected = false;
 
     void Start()
     {
@@ -24,7 +27,7 @@ public class TrollController : MonoBehaviour
 
     void Update()
     {
-        if (isWalking)
+        if (isWalking && !isPlayerDetected)
         {
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
@@ -34,11 +37,51 @@ public class TrollController : MonoBehaviour
     {
         while (true)
         {
-            isWalking = Random.value > 0.5f;
-            animator.SetBool(isWalkingParameter, isWalking);
+            if (!isPlayerDetected)
+            {
+                isWalking = Random.value > 0.5f;
+                animator.SetBool(isWalkingParameter, isWalking);
 
-            float waitTime = Random.Range(minWaitTime, maxWaitTime);
-            yield return new WaitForSeconds(waitTime);
+                float waitTime = Random.Range(minWaitTime, maxWaitTime);
+                yield return new WaitForSeconds(waitTime);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerDetected = true;
+            StartCoroutine(AttackPlayer());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerDetected = false;
+            animator.SetBool(attackParameter, false);
+            animator.SetBool(isWalkingParameter, false);
+            StopCoroutine(AttackPlayer());
+        }
+    }
+
+    IEnumerator AttackPlayer()
+    {
+        while (isPlayerDetected)
+        {
+            animator.SetBool(attackParameter, true);
+
+            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+
+            animator.SetBool(attackParameter, false);
+            yield return new WaitForEndOfFrame();
         }
     }
 }
